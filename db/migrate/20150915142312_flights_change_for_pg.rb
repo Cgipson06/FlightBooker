6345faa5f7684adda07b6f963cca9851d7dd5d5c
Cody Gipson <CGipson06@yahoo.com>
2015-09-15 09:45:30 -0500

    heroku mess

--- db/migrate/20150915142312_flights_change_for_pg.rb
+++ db/migrate/20150915142312_flights_change_for_pg.rb
@@ -1,5 +1,5 @@
 class FlightsChangeForPg < ActiveRecord::Migration
   def change
-    change_column :flights, :duration, 'integer USING CAST(column_name AS integer)'
+    change_column :flights, :duration, 'integer USING CAST(duration AS integer)'
   end
 end
